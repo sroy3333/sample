9f87fe6cb013e01196598319d2363a033ce5119f
sroy3333 <sukanyaindia2222@gmail.com>
2023-12-19 00:27:29 +0530

    FruitShop(2)

--- demo.js
+++ demo.js
@@ -9,4 +9,4 @@ fruit[2].style.backgroundColor = 'yellow';
 fruit[0].style.fontWeight = 'bold';
 fruit[1].style.fontWeight = 'bold';
 fruit[2].style.fontWeight = 'bold';
-fruit[3].style.fontWeight = 'bold';
+fruit[3].style.fontWeight = 'bold'
