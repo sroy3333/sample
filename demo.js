2acea9060f422cfe556c70842bf62d4d4c721a43
sroy3333 <sukanyaindia2222@gmail.com>
2023-12-19 00:04:53 +0530

    Fruit Shop(2)

---
+++ demo.js
@@ -0,0 +1,12 @@
+const fruit = document.getElementsByClassName('fruit');
+console.log(fruit[0]);
+console.log(fruit[1]);
+console.log(fruit[2]);
+console.log(fruit[3]);
+
+
+fruit[2].style.backgroundColor = 'yellow';
+fruit[0].style.fontWeight = 'bold';
+fruit[1].style.fontWeight = 'bold';
+fruit[2].style.fontWeight = 'bold';
+fruit[3].style.fontWeight = 'bold';
