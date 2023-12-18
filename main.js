e83c8098743ffcf39b93e73cc81b4463e456a157
sroy3333 <sukanyaindia2222@gmail.com>
2023-12-18 23:31:11 +0530

    Fruit Shop code

---
+++ main.js
@@ -0,0 +1,13 @@
+const mainHeading = document.getElementById('main-heading');
+mainHeading.textContent = 'Fruit World';
+mainHeading.style.color = 'orange';
+
+const header = document.getElementById('header');
+header.style.backgroundColor = 'green';
+header.style.borderBottom = '3px solid orange';
+
+const basketHeading = document.getElementById('basket-heading');
+basketHeading.style.color = 'green';
+
+const thanks = document.getElementById('thanks');
+thanks.innerHTML = '<p>Please visit us again.</p>';
